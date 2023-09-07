612356e79f63bd5a71b527a11cd30d8dc3cdde69
Vanessa Estival <vel@ehtrace.com>
2023-09-07 15:25:39 +0200

    Not working cards v1

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,79 @@
+<script setup lang="ts">
+import PokemonCard from "@/components/PokemonCard.vue";
+
+import axios from 'axios'
+import { useAxios } from '@vueuse/integrations/useAxios'
+import {Pokemon} from "@/typedef/pokemon";
+
+const instance = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2',
+})
+
+const { data: pokemonData, isFinished } = useAxios('/pokemon', { method: 'GET' }, instance)
+
+let pokemons: Pokemon[] = []
+
+</script>
+
 <template>
-  <v-app>
+  <v-app class="ma-8">
+    <v-app-bar scroll-behavior="elevate" class="ma-2 pa-8">
+      <v-container>
+        <v-row>
+          <v-col class="d-inline-flex">
+            <v-switch label="Switch" inset></v-switch>
+            <v-img height="2em" src="./assets/pokedex.png"></v-img>
+            <v-icon
+              icon="mdi-github"
+            ></v-icon>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
     <v-main>
-
-      <header>
-        <div class="d-flex">
-          <v-switch label="Switch" inset></v-switch>
-          <v-img src="./assets/pokedex.png"></v-img>
-          <v-icon
-            icon="mdi-github"
-          ></v-icon>
-        </div>
-        <div class="d-flex">
-          <v-autocomplete
-            clearable
-            label="Regions"
-            :items="['Kanto (1-151)', 'Johto (152-251)', 'Hoenn (252-386)', 'Sinnoh (387-494)', 'Unova (495-649)', 'Kalos (650-721)', 'Alola (722-809)', 'Galar (810-898)']"
-            multiple
-          ></v-autocomplete>
-          <v-autocomplete
-            clearable
-            label="Types"
-            :items="['Grass', 'Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water']"
-            multiple
-          ></v-autocomplete>
-          <v-autocomplete
-            clearable
-            label="Sort by"
-            :items="['Name', 'ID']"
-            multiple
-          ></v-autocomplete>
-          <v-autocomplete
-            class="flex-full-width"
-            density="comfortable"
-            menu-icon=""
-            placeholder="Search a Pokemon"
-            prepend-inner-icon="mdi-magnify"
-            rounded
-            theme="light"
-            variant="solo"
-          ></v-autocomplete>
-        </div>
-      </header>
-      <main>
-
-      </main>
+      <v-container class="ma-2 pa-8 ma-auto">
+        <v-row>
+          <v-col>
+            <v-autocomplete
+              clearable
+              label="Regions"
+              :items="['Kanto (1-151)', 'Johto (152-251)', 'Hoenn (252-386)', 'Sinnoh (387-494)', 'Unova (495-649)', 'Kalos (650-721)', 'Alola (722-809)', 'Galar (810-898)']"
+              multiple
+            ></v-autocomplete>
+          </v-col>
+          <v-col>
+            <v-autocomplete
+              clearable
+              label="Types"
+              :items="['Grass', 'Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water']"
+              multiple
+            ></v-autocomplete>
+          </v-col>
+          <v-col>
+            <v-autocomplete
+              clearable
+              label="Sort by"
+              :items="['Name', 'ID']"
+              multiple
+            ></v-autocomplete>
+          </v-col>
+          <v-col>
+            <v-autocomplete
+              class="flex-full-width"
+              density="comfortable"
+              menu-icon=""
+              placeholder="Search a Pokemon"
+              prepend-inner-icon="mdi-magnify"
+              rounded
+              theme="light"
+              variant="solo"
+            ></v-autocomplete>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-container>
+        <PokemonCard v-for="(pokemon, index) in pokemons" :key="index" :pokemon="pokemon"></PokemonCard>
+      </v-container>
     </v-main>
   </v-app>
 </template>
-
-<script setup lang="ts">
-
-</script>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+  pokemon: any
+}>()
+
+const getPokemonImgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${props.pokemon.id}.png`
+
+</script>
+
+<template>
+  <v-card class="mx-auto"
+          max-width="344"
+          >
+    <v-card-title>ID: {{ props.pokemon.id }}</v-card-title>
+    <v-img
+        :src="getPokemonImgUrl"
+        height="200px"
+        cover
+    ></v-img>
+    <v-card-subtitle>{{ props.pokemon.name }}</v-card-subtitle>
+    <v-icon>{{ props.pokemon.type }}</v-icon>
+    <v-card-actions>
+      <v-btn color="orange-lighten-2" variant="text">
+        Infos
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
